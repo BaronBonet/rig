56cbec0e259ab955cc494745f3107708d260248b
Eric Bonet <ericcbonet@gmail.com>
2026-04-13 10:21:10 +0200

    chore: get CI go cache to work

--- scripts/dependencies/install.sh
+++ scripts/dependencies/install.sh
@@ -3,21 +3,14 @@ set -euo pipefail
 
 DIR_PATH=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
 
-. "${DIR_PATH}/../../build_dependencies_versions"
-
 DEST_DIR="${DIR_PATH}/../../local/bin"
-TMP_DIR="${DIR_PATH}/../../local/tmp"
 
 mkdir -p "${DEST_DIR}"
-mkdir -p "${TMP_DIR}"
-
-echo "Installing golangci-lint"
-TMP_GOBIN=$(mktemp -d "${TMP_DIR}/golangci-lint.XXXXXX")
-trap 'rm -rf "${TMP_GOBIN}"' EXIT
-GOBIN="${TMP_GOBIN}" go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v${GOLANGCI_LINT_VERSION}
-mv -f "${TMP_GOBIN}/golangci-lint" "${DEST_DIR}/golangci-lint"
 
 echo "Downloading Go module and tool dependencies..."
 go mod download
+
+echo "Installing golangci-lint"
+GOBIN="${DEST_DIR}" go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint
 
 echo "All dependencies installed! ✨"
